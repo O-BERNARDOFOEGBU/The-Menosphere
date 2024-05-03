0d503bfc4479540fa60a8c750e655126c9d14b36
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2024-05-03 13:15:38 +0100

    layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../images/logo.png";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+
+const Header = () => {
+  return (
+    <nav>
+      <div className="container nav__container">
+        <Link to="/" className="nav__logo">
+          <img src={Logo} alt="Navbar Logo" />
+        </Link>
+        <ul className="nav__menu">
+          <li>
+            <Link to="/profile">Bernard Ofoegbu</Link>
+          </li>
+          <li>
+            <Link to="/create">Create Post</Link>
+          </li>
+          <li>
+            <Link to="/authors">Authors</Link>
+          </li>
+          <li>
+            <Link to="Logout">Logout</Link>
+          </li>
+          <button className="nav__toggle-btn">
+            <AiOutlineClose />
+          </button>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- src/pages/EditPosts.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EditPosts = () => {
-  return <div>EditPosts</div>;
-};
-
-export default EditPosts;
